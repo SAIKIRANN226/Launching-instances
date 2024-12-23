d6acb3e9b895a785dd0e2d7bacd49c9415af8192
mothukuru saikiran <obscuresai.com>
2024-12-23 12:22:27 +0530

    message

--- shell.sh
+++ shell.sh
@@ -4,12 +4,12 @@ NUMBER=$1
 R="\e[31m"
 N="\e[0m"
 
-if [ $NUMBER -gt 100 ]
-then 
-    echo -e "$R Given number is greater than 100 $N"
-else
-    echo "Given number is lessthan 100"
-fi
+# if [ $NUMBER -gt 100 ]
+# then 
+#     echo -e "$R Given number is greater than 100 $N"
+# else
+#     echo "Given number is lessthan 100"
+# fi
 
 
 a=9
