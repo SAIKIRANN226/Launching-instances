feb31851d2f29f4ef39d0a857fd959f5cea62b0e
mothukuru saikiran <obscuresai.com>
2024-12-23 16:41:29 +0530

    message

--- shell.sh
+++ shell.sh
@@ -1,5 +1,5 @@
 EC2_NAME="sai_ec2"  
-AMI_ID="ami-0b4f379183e5706b9"  
+AMI_NAME="Centos-8-DevOps-Practice" 
 INSTANCE_TYPE="t2.micro"     
 STORAGE_SIZE=8               
 SECURITY_GROUP_ID="sg-062184d660bab16ba"
