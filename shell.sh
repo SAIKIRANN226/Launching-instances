29531d6fe289873cfeef7d64b1788c0f836513af
mothukuru saikiran <obscuresai.com>
2024-12-23 11:46:59 +0530

    message

--- shell.sh
+++ shell.sh
@@ -1,38 +1,12 @@
 #!/bin/bash
 
-ID=$(id -u)
-DATE=$(date)
+NUMBER=$1
 R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
 N="\e[0m"
 
-VALIDATE() {
-    if [ $1 -ne 0 ]
-    then 
-        echo -e "$2.....$R FAILED $N"
-        exit 1
-    else
-        echo -e "$2.....$G SUCCESS $N"
-    fi 
-}
-
-if [ $ID -ne 0 ]
+if [ $NUMBER -gt 100 ]
 then 
-    echo -e "$R ERROR:: Please run the script with root user $N"
-    exit 1
+    echo -e "$R Given number is greater than 100 $N"
 else
-    echo -e "$Y Script started executing at $DATE $N"
-fi 
-
-for package in $@
-do 
-    yum list installed $package &>> temp.log
-    if [ $? -ne 0 ]
-    then 
-        yum install $package -y &>> temp.log
-        VALIDATE $? "Installation of $package"
-    else
-        echo -e "$package is already installed so .....$Y SKIPPING $N"
-    fi
-done
+    echo "Given number is lessthan 100"
+fi
