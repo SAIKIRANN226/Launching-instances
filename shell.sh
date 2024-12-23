0468a09f9465eba6daf1a1bbf69d1f44e98c9ba7
mothukuru saikiran <obscuresai.com>
2024-12-23 16:14:49 +0530

    message

--- shell.sh
+++ shell.sh
@@ -1,51 +1,25 @@
 #!/bin/bash
 
-# Ensure correct number of arguments are passed
-if [ "$#" -ne 1 ]; then
-  echo "Usage: $0 <ec2_name>"
-  exit 1
-fi
+EC2_NAME="sai_ec2"   
+AMI_ID="ami-0b4f379183e5706b9"  
+INSTANCE_TYPE="t2.micro"      
+STORAGE_SIZE=8               
+SECURITY_GROUP_ID="sg-062184d660bab16ba"  
 
-# Get arguments
-EC2_NAME=$1
+# Block device mapping for the root volume
+# BLOCK_DEVICE_MAPPING="DeviceName=/dev/sda1,Ebs={VolumeSize=$STORAGE_SIZE}"
 
-# Community AMI name (Centos-8-DevOps-Practice)
-AMI_NAME="Centos-8-DevOps-Practice"
-
-# Instance type (t2.micro)
-INSTANCE_TYPE="t2.micro"
-
-# EBS volume size (30 GiB)
-STORAGE_SIZE=30
-
-# Find the AMI ID based on the provided AMI name
-AMI_ID=$(aws ec2 describe-images \
-  --filters "Name=name,Values=$AMI_NAME" \
-  --query "Images[0].ImageId" \
-  --output text)
-
-# Check if AMI was found
-if [ "$AMI_ID" == "None" ]; then
-  echo "AMI with the name '$AMI_NAME' not found."
-  exit 1
-else
-  echo "Found AMI: $AMI_ID"
-fi
-
-# Launch the EC2 instance using the provided details
 echo "Launching EC2 instance with name: $EC2_NAME"
 
 INSTANCE_ID=$(aws ec2 run-instances \
   --image-id "$AMI_ID" \
   --instance-type "$INSTANCE_TYPE" \
-  --block-device-mappings "DeviceName=/dev/sda1,Ebs={VolumeSize=$STORAGE_SIZE}" \
-  --count 1 \
+  --security-group-ids "$SECURITY_GROUP_ID" \
   --associate-public-ip-address \
-  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$EC2_NAME}]" \
   --query 'Instances[0].InstanceId' \
   --output text)
 
-# Check if instance creation was successful
+
 if [ "$INSTANCE_ID" == "None" ]; then
   echo "Failed to launch EC2 instance."
   exit 1
