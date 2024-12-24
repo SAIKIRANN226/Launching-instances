cf1774d734ee62b5512d45c75fe77554b8de6ddf
mothukuru saikiran <obscuresai.com>
2024-12-24 10:36:19 +0530

    message

--- t3.medium.sh
+++ t3.medium.sh
@@ -1,32 +1,37 @@
 #!/bin/bash
 
-EC2_NAME="third"  
-AMI_NAME="Centos-8-DevOps-Practice" 
-INSTANCE_TYPE="t3.medium"     
-STORAGE_SIZE=30               
-SECURITY_GROUP_ID="sg-062184d660bab16ba"
+# Variables
+AMI_NAME="Centos-8-DevOps-Practice"  
+INSTANCE_TYPE="t3.medium"            
+SECURITY_GROUP="ssg-062184d660bab16ba" 
+REGION="us-east-1"                  
+VOLUME_SIZE=30
 
 
 AMI_ID=$(aws ec2 describe-images \
   --filters "Name=name,Values=$AMI_NAME" \
   --query "Images[0].ImageId" \
-  --output text)
+  --output text \
+  --region $REGION)
+
 
 if [ "$AMI_ID" == "None" ]; then
-  echo "AMI '$AMI_NAME' not found."
+  echo "Error: AMI '$AMI_NAME' not found in region $REGION."
   exit 1
-else
-  echo "Found AMI: $AMI_ID"
 fi
 
-echo "Launching EC2 instance with name: $EC2_NAME"
 
-
+echo "Launching EC2 instance..."
 INSTANCE_ID=$(aws ec2 run-instances \
-  --image-id "$AMI_ID" \
-  --instance-type "$INSTANCE_TYPE" \
-  --security-group-ids "$SECURITY_GROUP_ID" \
-  --associate-public-ip-address \
+  --image-id $AMI_ID \
+  --instance-type $INSTANCE_TYPE \
+  --security-group-ids $SECURITY_GROUP \
+  --no-key-name \
+  --region $REGION \
+  --count 1 \
+  --block-device-mappings DeviceName=/dev/sda1,Ebs={VolumeSize=$VOLUME_SIZE} \
+  --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=MyDevOpsInstance}]' \
+  --query 'Instances[0].InstanceId' \
   --output text)
 
-# ./<scrip-name>.sh to run the script
+
