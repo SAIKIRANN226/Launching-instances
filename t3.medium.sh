67cf6056e816cdbc9af51c7c051c28b1ef069c56
mothukuru saikiran <obscuresai.com>
2024-12-24 11:02:06 +0530

    message

--- t3.medium.sh
+++ t3.medium.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
+AMI_ID=$(aws ec2 describe-images \
+  --filters "Name=name,Values=Centos-8-DevOps-Practice" \
+  --query "Images[0].ImageId" \
+  --output text)
+
+
+if [ "$AMI_ID" == "None" ]; then
+  echo "Error: AMI 'Centos-8-DevOps-Practice' not found."
+  exit 1
+fi
+
+
 INSTANCE_ID=$(aws ec2 run-instances \
-  --image-id $(aws ec2 describe-images --filters "Name=name,Values=Centos-8-DevOps-Practice" --query "Images[0].ImageId" --output text) \
+  --image-id $AMI_ID \
   --instance-type t3.medium \
   --security-group-ids ssg-062184d660bab16ba \
   --no-key-name \
@@ -11,4 +23,3 @@ INSTANCE_ID=$(aws ec2 run-instances \
   --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=EC2-1}]' \
   --query 'Instances[0].InstanceId' \
   --output text)
-
